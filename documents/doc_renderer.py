6a39e38ef67b549c17c5964b8f7a20f56fc57da5
Branislav Holländer <branislav.hollander@ge.com>
2018-05-04 14:20:55 +0200

    Transform pdf2txt (html2txt conversion preparation)

--- documents/doc_renderer.py
+++ documents/doc_renderer.py
@@ -5,32 +5,7 @@ include rendering of HTML files and others.
 
 import asyncio
 import os
-from pyppeteer import launch
 import subprocess
-
-def render_html(file_path, output_filename):
-    """Render html file to a specific format.
-    Input params:
-    file_path: HTML document to render.
-    output_path: Name of the output file. The output
-    will be saved in the same directory as the input file.
-    """
-    output_noext, format = os.path.splitext(output_filename)
-    if (format != '.png'):
-        print('Forcing output format of html file to png')
-        format = '.png'
-
-    path, filename = os.path.split(file_path)
-    output_path = os.path.join(path, output_noext + format)
-
-    async def make_screenshot():
-        browser = await launch()
-        page = await browser.newPage()
-        await page.goto(file_path)
-        await page.screenshot({'path': output_path, 'fullPage': True})
-        await browser.close()
-
-    asyncio.get_event_loop().run_until_complete(make_screenshot())
 
 def _run_pdfrenderer(args, cwd):
     """Calls the renderer with the appropriate
@@ -70,10 +45,10 @@ def render_pdf(file_path, output_filename, resolution):
     args = ['-q',resolutionstr,sdevicestr,compression,outputfilestr,filename,'-c','quit']
     _run_pdfrenderer(args, cwd=cwd)
 
-def render_doc(document_path, output_filename, format, resolution):
-    if format == 'pdf':
+def render_doc(document_path, output_filename, resolution):
+    _, format = os.path.splitext(document_path)
+
+    if format == '.pdf':
         render_pdf(document_path, output_filename, resolution)
-    elif format == 'html':
-        render_html(document_path, output_filename)
     else:
-        raise ValueError('Unsupported document format')
+        raise ValueError('Unsupported document format for rendering')
--- extract_text_curia_docs.py
+++ extract_text_curia_docs.py
@@ -1,5 +1,12 @@
+"""This script tries to extract text and high-level structure from
+documents. It does this by batch-processing all documents belonging
+to all cases. For HTML documents, a text parser is called.
+PDF documents are converted to an image format first (e.g. tiff) and
+then processed with tesseract-ocr.
+"""
+
 from database.database import CURIACaseDatabase
-from documents import doc_textextractor
+from documents import doc_textextractor, doc_renderer
 import helpers
 from pathlib import Path
 from tqdm import tqdm
@@ -10,13 +17,24 @@ cases = db.get_all_cases()
 def text_from_docs_for_case(case, docs):
     """Extract text from documents in a case.
     """
-    output_format = 'json' # json is the only supported text document format
+    output_format = 'txt' # txt is the only supported text document format
     folder_path = Path('documents/' + helpers.case_name_to_folder(case['name']))
+
     for doc in docs:
         doc_filename = str(doc['id']) + '.' + doc['format']
+        doc_path = str(folder_path / doc_filename)
         output_filename = str(doc['id']) + '.' + output_format
 
-        doc_textextractor.extract_text(str(folder_path / doc_filename), output_filename)
+        if doc['format'] == 'pdf':
+            # first convert pdf to tiff image
+            img_filename = str(doc['id']) + '.tiff'
+            doc_renderer.render_doc(doc_path, img_filename, 300)
+
+            # extract text from tiff
+            doc_textextractor.extract_text(str(folder_path / img_filename), output_filename)
+        elif doc['format'] == 'html':
+            output_filename = str(doc['id']) + '.' + output_format
+            doc_textextractor.extract_text(str(folder_path / doc_filename), output_filename)
 
 for case in tqdm(cases):
     docs = db.get_docs_for_case(case, only_valid=True)
