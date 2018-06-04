38ac7b309ce09a455406d7b974bb3025e594bd00
Branislav Holländer <branislav.hollander@ge.com>
2018-06-04 15:17:49 +0200

    Fix phrases formation

--- nlp/phrases.py
+++ nlp/phrases.py
@@ -9,23 +9,26 @@ def build_phrases_regex(document, rules):
     # try regex rules on bigrams and connect them if rules apply
     phrases_document = []
     for sentence in document:
-        last_word = None
         phrases = []
-        for word in sentence:
-            matched = False
-            if last_word is not None:
-                phrase = last_word + ' ' + word # this can sometimes reach across sentences
+        if len(sentence) < 2:
+            phrases = sentence
+        else:
+            last_word = sentence[0]
+            i = 1
+            while i < len(sentence):
                 matched = False
-                for rule in rules:
+                word = sentence[i]
+                last_word = sentence[i-1]
+                phrase = last_word + ' ' + word
+                for rule in rules: # try to match all rules
                     if re.match(rule, phrase):
                         phrases.append(phrase)
+                        i += 2
                         matched = True
                         break
                 if not matched:
                     phrases.append(last_word)
-            if matched:
-                last_word = None
-            else:
-                last_word = word
+                    i += 1
+
         phrases_document.append(phrases)
     return phrases_document
