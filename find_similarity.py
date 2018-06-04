e5ac3a137a5e2bf94e2b17bd33f59aa06160fb43
Branislav Holländer <branislav.hollander@ge.com>
2018-06-04 09:29:31 +0200

    New rules for similarity

--- find_similarity.py
+++ find_similarity.py
@@ -16,7 +16,13 @@ print('Loading model...')
 model = Word2Vec(vocabulary)
 model.load(save_path)
 
-rules = [r"articl \d+\w*"]
+rules = [r"articl \d+\w*",
+         r"paragraph \d+\w*",
+         r"law no",
+         r"law no \d+\w*",
+         r"direct \d+\w*",
+         r"^((31(?!\ (feb(ruary)?|apr(il)?|june?|(sep(?=\b|t)t?|nov)(emb)?)))|((30|29)(?!\ feb(ruary)?))|(29(?=\ feb(ruary)?\ (((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)))))|(0?[1-9])|1\d|2[0-8])\ (jan(uary)?|feb(ruary)?|ma(r(ch)?|y)|apr(il)?|ju((li?)|(ne?))|aug(ust)?|oct(ob)?|(sep(?=\b|t)t?|nov|dec)(emb)?)$",
+         r"^((31(?!\ (feb(ruary)?|apr(il)?|june?|(sep(?=\b|t)t?|nov)(emb)?)))|((30|29)(?!\ feb(ruary)?))|(29(?=\ feb(ruary)?\ (((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)))))|(0?[1-9])|1\d|2[0-8])\ (jan(uary)?|feb(ruary)?|ma(r(ch)?|y)|apr(il)?|ju((li?)|(ne?))|aug(ust)?|oct(ob)?|(sep(?=\b|t)t?|nov|dec)(emb)?)\ ((1[6-9]|[2-9]\d)\d{2})$"]
 
 docs = table_docs.get_docs_with_name('Judgment')
 
--- train_model_curia.py
+++ train_model_curia.py
@@ -34,7 +34,13 @@ helpers.create_folder_if_not_exists('trained_models')
 save_path = os.path.join('trained_models', helpers.setup_json['model_path'])
 
 print('Initializing phrases...')
-rules = [r"articl \d+\w*"]
+rules = [r"articl \d+\w*",
+         r"paragraph \d+\w*",
+         r"law no",
+         r"law no \d+\w*",
+         r"direct \d+\w*",
+         r"^((31(?!\ (feb(ruary)?|apr(il)?|june?|(sep(?=\b|t)t?|nov)(emb)?)))|((30|29)(?!\ feb(ruary)?))|(29(?=\ feb(ruary)?\ (((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)))))|(0?[1-9])|1\d|2[0-8])\ (jan(uary)?|feb(ruary)?|ma(r(ch)?|y)|apr(il)?|ju((li?)|(ne?))|aug(ust)?|oct(ob)?|(sep(?=\b|t)t?|nov|dec)(emb)?)$",
+         r"^((31(?!\ (feb(ruary)?|apr(il)?|june?|(sep(?=\b|t)t?|nov)(emb)?)))|((30|29)(?!\ feb(ruary)?))|(29(?=\ feb(ruary)?\ (((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)))))|(0?[1-9])|1\d|2[0-8])\ (jan(uary)?|feb(ruary)?|ma(r(ch)?|y)|apr(il)?|ju((li?)|(ne?))|aug(ust)?|oct(ob)?|(sep(?=\b|t)t?|nov|dec)(emb)?)\ ((1[6-9]|[2-9]\d)\d{2})$"]
 phrases = [build_phrases_regex(doc, rules=rules) for doc in document_gen]
 
 print('Initializing vocabulary...')
