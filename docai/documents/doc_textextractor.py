2249f4c0987f9c819a30b2b7998f80fa2877f5d2
Branislav Holländer <branislav.hollander@ge.com>
2018-06-28 15:22:21 +0200

    Fix doc_textextractor not reading the whole file

--- docai/documents/doc_textextractor.py
+++ docai/documents/doc_textextractor.py
@@ -29,6 +29,6 @@ def extract_from_image(file_path):
 def extract_from_html(file_path):
     with open(os.path.join(file_path), 'r', encoding='utf-8') as file:
         html = BeautifulSoup(file, 'html.parser')
-        text = html.body.get_text(separator='\n')
+        text = html.get_text(separator='\n')
         text = text.strip()
     return text
